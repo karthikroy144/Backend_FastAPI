83a84f5c83a6ca90cd63f5e319fc362679f361d6
Karthik Roy <karthikroy@example.com>
2025-12-24 10:34:16 +0530

    commit

--- database_models.py
+++ database_models.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class Product(Base):
     __tablename__ = 'product'
     id = Column(Integer, primary_key=True, index=True)
